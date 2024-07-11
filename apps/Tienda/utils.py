9f2f4eb26f1b0774817f47a05f163416c00e7102
MartinQuilodran <127960542+MartinQuilodran@users.noreply.github.com>
2024-07-11 10:48:21 -0400

    Update utils.py

--- apps/Tienda/utils.py
+++ apps/Tienda/utils.py
@@ -1,10 +1,23 @@
 import requests
+from requests.exceptions import RequestException
+import json
+
 
 def enviar_solicitud_transbank(url, data):
     headers = {
-        'Tbk-Api-Key-Id': '597055555532',
-        'Tbk-Api-Key-Secret': '579B532A7440BB0C9079DED94D31EA1615BACEB56610332264630D42D0A36B1C',
         'Content-Type': 'application/json',
+        'Tbk-Api-Key-Id': '597055555532',  # Reemplaza con tu API Key ID
+        'Tbk-Api-Key-Secret': '579B532A7440BB0C9079DED94D31EA1615BACEB56610332264630D42D0A36B1C'  # Reemplaza con tu API Key Secret
     }
-    response = requests.post(url, json=data, headers=headers)
-    return response.json()
+
+    try:
+        # Debug: Imprimir datos de la solicitud
+        print(f"Enviando datos a Transbank: {json.dumps(data, indent=2)}")
+        
+        response = requests.post(url, headers=headers, data=json.dumps(data))
+        response.raise_for_status()  # Esto lanzará un error si el código de estado HTTP es 4xx o 5xx
+        return response.json()
+    except RequestException as e:
+        print(f"Error en la solicitud a Transbank: {e}")
+        return {"error": str(e), "status_code": response.status_code if response else "No Response"}
+
